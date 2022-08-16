8fa962714ec493c75d6333e597e503f184db95f5
Archivopdf <toni.diaz.gar77@gmail.com>
2022-08-16 18:32:44 +0200

    first

--- Reto1.js
+++
@@ -1,7 +0,0 @@
-console.log("Mensaje 1");
-setTimeout(() => {
-    console.log("Mensaje 2");
-    setTimeout(() => {
-        console.log("Mensaje 3");
-    });
-}, 3000);
---
+++ Reto3.js
@@ -0,0 +1,6 @@
+const person = {
+    name: "",
+    surname: "",
+    age: ""
+};
+
