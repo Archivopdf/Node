142e5ef8b5dce789b6135cea2ef050346c996edc
Archivopdf <toni.diaz.gar77@gmail.com>
2022-08-17 01:49:41 +0200

    update

--- Reto2.js
+++ Reto2.js
@@ -7,13 +7,24 @@ let person = {
     age: 27
 };
 
-fs.writeFile('persona.json', JSON.stringify(person))
-    .then(() => {
-        return fs.readFile('person.json', 'utf8')
-    })
-    .then(data => {
-        console.log(JSON.parse(person));
-    })
-    .catch(err => {
-        console.log(err);
-    })
+async function asyncAwait() {
+
+    try {
+
+        fs.writeFile('persona.json', JSON.stringify(person))
+            .then(() => {
+                return fs.readFile('person.json', 'utf8')
+            })
+            .then(data => {
+                console.log(JSON.parse(person));
+            })
+        // .catch(err => {
+        //     console.log(err);
+        // })
+
+    } catch (err) {
+        console.err('Something wrong happened removing the file');
+    }
+}
+
+asyncAwait()
